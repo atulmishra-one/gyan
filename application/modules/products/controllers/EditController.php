3a8350f284f7379ab60f1bf04b02f088e8b04cc9
atulmishra-one <atulmishra.one@gmail.com>
2014-04-20 22:51:08 +0530

    added product list view

--- application/modules/products/controllers/EditController.php
+++ application/modules/products/controllers/EditController.php
@@ -12,7 +12,45 @@ class Products_EditController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $id = $this->getParam('id');
+        
+        $productsM = new Application_Model_ProductsMapper();
+        $this->view->products = $productsM->fetchAll($id);
+        
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            if( ( $this->validate( $request->getPost() ) ) === true ) {
+                
+                //print_r($request->getPost());
+                $products = new Application_Model_Products( $request->getPost() );
+                $productsM = new Application_Model_ProductsMapper();
+                
+                $productsM->save($products);
+                $this->_helper->flashMessenger->addMessage('<div class="alert alert-success">Success !</div>');
+            }
+        }
+        
+        $this->view->messages = array_merge(
+                $this->_helper->flashMessenger->getMessages(), $this->_helper->flashMessenger->getCurrentMessages()
+        );
+        
+        $this->_helper->flashMessenger->clearCurrentMessages();
+    }
+    
+    protected function validate( $data ) {
+        
+        if ( empty( $data['name']) ) {
+            $this->_helper->flashMessenger->addMessage('<div class="alert alert-danger">Please provide name !</div>');
+            return;
+        }
+        
+        if ( empty( $data['price']) ) {
+            $this->_helper->flashMessenger->addMessage('<div class="alert alert-danger">Please provide price !</div>');
+            return;
+        }
+        
+        return true;
     }
 
 
